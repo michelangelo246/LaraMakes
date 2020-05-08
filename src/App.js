f6b55a5be139f63ea897ddaba9c42a92d2789ccf
Michelangelo da Rocha Machado <michelangelo246@hotmail.com>
2020-05-07 22:52:55 -0300

    add header component and links to home, shop and contact. Plus adjust body padding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route
-          exact
-          path='/'
-          component={HomePage}
-        />
-        <Route
-          exact
-          path='/shop'
-          component={ShopPage}
-        />
-      </Switch>
+      <Header/>
+      <div className="body-template">
+        <Switch>
+          <Route
+            exact
+            path='/'
+            component={HomePage}
+          />
+          <Route
+            exact
+            path='/shop'
+            component={ShopPage}
+          />
+        </Switch>
+      </div>
     </div>
   );
 }
