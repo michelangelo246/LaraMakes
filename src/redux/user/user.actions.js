fc583c4920ff7b3f9dc8b6c5d19e2524f1972fe6
Michelangelo da Rocha Machado <michelangelo246@hotmail.com>
2020-05-16 02:53:44 -0300

    update app to redirect to home if user is signin; modularize actions with user.types.js

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
+import { UserActionTypes } from './user.types'
+
 // aqui, definimos o nome da ação e coletamos os dados necessários para a ação,
 // os quais serão passados ao reducer responsável
 
 export const setCurrentUser = user => ({
-   type: 'SET_CURRENT_USER',
+   type: UserActionTypesSET_CURRENT_USER,
    payload: user
 });
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = { // esse é o estado inicial do reducer, similar ao 'this.state =' do construtor
    currentUser: null
 }
@@ -6,7 +8,7 @@ const INITIAL_STATE = { // esse é o estado inicial do reducer, similar ao 'this
 // aqui, de acordo com a ação, realizamos as operações necessárias e atualizamos o state do app
 const userReducer = (state = INITIAL_STATE, action) => { // define valor padrão para state
    switch(action.type){
-      case 'SET_CURRENT_USER':
+      case UserActionTypes.SET_CURRENT_USER:
          return {
             ...state, // espalha os demais atributos do estado no novo estado-objeto retornado
             currentUser: action.payload // altera somente o desejado; deve vir sempre após ...state, (para sobreescrever)
